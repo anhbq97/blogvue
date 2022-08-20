1a783266f36d0561283fa6d313f4c454f55013fa
Quoc Anh Bui <anhbq.it97@gmail.com>
2022-08-20 18:24:51 +0700

    update

--- resources/js/vue/home/index.vue
+++ resources/js/vue/home/index.vue
@@ -1,38 +1,25 @@
 <template>
-    <div>
-        <p>Home</p>
-    </div>
-    <div class="site-wrapper">
-        <div class="header-wrap">
-            <div class="container">
-                <Menu></Menu>
+    <main class="container px-xs-0" id="main-body-content">
+        <div class="row px-xs-0">
+            <div class="col-md-12">
+                <Posts></Posts>
             </div>
         </div>
+    </main>
 
-        <main class="container px-xs-0" id="main-body-content">
-            <div class="row px-xs-0">
-                <div class="col-md-12">
-                    <Posts></Posts>
-                </div>
-            </div>
-        </main>
-
-        <footer>
-            <div class="container">
-                <Footer></Footer>
-            </div>
-        </footer>
-    </div>
+    <footer>
+        <div class="container">
+            <Footer></Footer>
+        </div>
+    </footer>
 </template>
 
 <script>
-    import Menu from '../home/includes/menu.vue';
     import Posts from '../home/includes/posts.vue';
     import Footer from '../home/includes/footer.vue';
     
     export default {
         components: {
-            Menu,
             Posts,
             Footer
         }
