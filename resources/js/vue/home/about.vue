1e8981a2c495325468e1e17003070708a3a0226a
Quoc Anh Bui <anhbq.it97@gmail.com>
2022-08-24 23:18:25 +0700

    update

---
+++ resources/js/vue/home/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <Menu />
+    <div>
+        <p>About</p>
+    </div>
+</template>
+
+<script>
+    import Menu from './includes/menu.vue';
+
+    export default {
+        components: {
+            Menu
+        }
+    }
+</script>
