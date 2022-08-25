d0ee80da1ab50d99b76e6b0f2b44532c0c0784f0
Quoc Anh Bui <anhbq.it97@gmail.com>
2022-08-25 10:59:53 +0700

    update

---
+++ resources/js/vue/layouts/navbars/guest/nav.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 my-3">
+  <div class="container-fluid">
+    <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3">
+      Soft UI Dashboard Laravel
+    </a>
+    <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon mt-2">
+        <span class="navbar-toggler-bar bar1"></span>
+        <span class="navbar-toggler-bar bar2"></span>
+        <span class="navbar-toggler-bar bar3"></span>
+      </span>
+    </button>
+    <div class="collapse navbar-collapse" id="navigation">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link me-2" href="">
+            <i class="fas fa-user-circle opacity-6 me-1 "></i>
+            Sign Up
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="">
+            <i class="fas fa-key opacity-6 me-1 "></i>
+            Sign In
+          </a>
+        </li>
+      </ul>
+      <ul class="navbar-nav d-lg-block d-none">
+        <li class="nav-item">
+          <a href="https://www.creative-tim.com/product/soft-ui-dashboard-laravel" target="_blank" class="btn btn-sm btn-round mb-0 me-1 bg-gradient-light">Free download</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+
