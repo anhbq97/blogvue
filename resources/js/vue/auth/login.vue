1c5e902f58af5045de877693ab51565312ae32ed
Quoc Anh Bui <anhbq.it97@gmail.com>
2022-09-30 23:41:42 +0700

    fix bug

--- resources/js/vue/auth/login.vue
+++ resources/js/vue/auth/login.vue
@@ -38,8 +38,8 @@ export default {
   
   data: function () {
     return {
-        email: 'bquocanh.97@gmail.com',
-        password: 'H40th13npr0.',
+        email: '',
+        password: '',
         message: '',
         code: 0
     }
