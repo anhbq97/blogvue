22fa84a04890dd8dd9566a85a143b4835eb0d2a1
Quoc Anh Bui <anhbq.it97@gmail.com>
2022-09-30 20:42:14 +0700

    update

--- resources/js/vue/router/index.vue
+++ resources/js/vue/router/index.vue
@@ -12,34 +12,43 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   // BLOG
-  { path: '/', component: Home, meta: { layout: 'LayoutBlog' },},
-  { path: '/about', component: About, meta: { layout: 'LayoutBlog' },},
-  { path: '/login', component: Login, meta: { layout: 'LayoutAuth' }, },
-  { path: '/register', component: Register, meta: { layout: 'layoutAuth' }, },
-  { path: '/post/:id', name: 'ReadPost', component: ReadPost, meta: { layout: 'LayoutBlog' }, },
+  { path: '/', component: Home, meta: { layout: 'LayoutBlog', requiresAuth: false }, name: 'Home'},
+  { path: '/about', component: About, meta: { layout: 'LayoutBlog', requiresAuth: false }, name: 'About'},
+  { path: '/login', component: Login, meta: { layout: 'LayoutAuth', requiresAuth: false }, name: 'Login'},
+  { path: '/register', component: Register, meta: { layout: 'layoutAuth', requiresAuth: false}, name: 'Register'},
+  { path: '/post/:id', component: ReadPost, meta: { layout: 'LayoutBlog', requiresAuth: false }, name: 'ReadPost'},
 
   // ERROR
-  { path: '/:pathMatch(.*)*', component: NotFound, meta: { layout: 'layoutAuth' },},
+  { path: '/:pathMatch(.*)*', component: NotFound, meta: { layout: 'layoutAuth', requiresAuth: false},},
 
   // ADMIN
   { 
     path: '/dashboard', 
     component: Dashboard, 
-    meta: { layout: 'LayoutAdmin' }, 
+    meta: { 
+      layout: 'LayoutAdmin', 
+      requiresAuth: true,
+    }, 
+    name: 'Dashboard'
   },
   {
     path: '/setting',
-    meta: { layout: 'LayoutAdmin' }, 
+    meta: { 
+      layout: 'LayoutAdmin', 
+      requiresAuth: true,
+    }, 
     children: [
       {
         path: '/setting/profile',
         component: SettingProfile,
-        meta: { layout: 'LayoutAdmin' }, 
+        meta: { layout: 'LayoutAdmin', requiresAuth: true,}, 
+        name: 'SettingProfile'
       },
       {
         path: '/setting/blog',
         component: SettingBlog,
-        meta: { layout: 'LayoutAdmin' }, 
+        meta: { layout: 'LayoutAdmin', requiresAuth: true, }, 
+        name: 'SettingBlog'
       },
     ],
   },
@@ -48,7 +57,23 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
+
+const auth = {
+  isLoggedIn: () => {
+    if (localStorage.getItem('isLogged')) { return true; }
+    return false;
+  }
+};
+
+router.beforeEach((to, from, next) => {
+  console.log(to.name);
+  if (to.meta.requiresAuth && !auth.isLoggedIn()) {
+    next({ name: 'Login' });
+  } else if (auth.isLoggedIn() && (to.name == 'Login' || to.name == 'Register')) {
+    next({ name: 'Dashboard'});
+  } else next();
+});
 
 export default router
 </script>
